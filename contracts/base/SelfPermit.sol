676bd4f5268c63475d5910f6318368be3a6bcd98
arhansuba <subasiarhan3@gmail.com>
2024-08-24 12:08:46 +0300

    commit 1

--- contracts/base/SelfPermit.sol
+++ contracts/base/SelfPermit.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity >=0.5.0;
+pragma solidity 0.8.26;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/drafts/IERC20Permit.sol';
